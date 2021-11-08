8fe1c45687636e276c5f88760918ed0b0338f595
romanlancer <roman.girich@gmail.com>
2021-11-08 22:06:16 +0000

    Deploying to gh-pages from @ romanlancer/TKdevelopers@34cb5f830360ec007f1854488b93cb2bb92645cd 🚀

--- src.22b7aa9c.js
+++ src.22b7aa9c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/TKdevelopers/src.c58cd183.js.map
+//# sourceMappingURL=/TKdevelopers/src.22b7aa9c.js.map
