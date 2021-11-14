21917eea0189127d41f7d8680760a395534eae10
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2021-11-14 20:55:13 +0200

    Done all
    Закончил полностью разобрал, и пофиксил вроде все что было в свайпере, полностью готово к статической сдаче

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -5,5 +5,19 @@ const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-  }
+    dynamicBullets: true,
+    clickable: true,
+    // type: 'custom',
+    // bulletClass: 'swiper_pagination_img',
+  },
+  autoplay: {
+    delay: 6000,
+    stopOnLastSlide: true,
+    disableOnInteraction: false,
+  },
+  effect: 'flip',
+  flipEffect: {
+    slideShadows: true,
+    limitRotation: true,
+  },
 });
