e866c9583d4c3b4371c497eed70af680fc2add71
Roman Hyrych <88936471+romanlancer@users.noreply.github.com>
2021-11-16 00:33:19 +0200

    header-hero modal fix + current page color

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,12 @@
 (() => {
 	const refs = {
 		openModalBtn: document.querySelector('[data-modal-open]'),
+		openModalBtnMenu: document.querySelector('[data-modal-menu-open]'),
 		closeModalBtn: document.querySelector('[data-modal-close]'),
 		modal: document.querySelector('[data-modal]'),
 	};
-
 	refs.openModalBtn.addEventListener('click', toggleModal);
+	refs.openModalBtnMenu.addEventListener('click', toggleModal);
 	refs.closeModalBtn.addEventListener('click', toggleModal);
 
 	function toggleModal() {
