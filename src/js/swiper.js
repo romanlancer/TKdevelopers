cfce835a82d6110b92aae46a2f663c63fd14eca8
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2021-11-16 12:34:58 +0200

    swiper
    Добавил свайпер работает нужно только придумать как кнопку привязать на прокрутку либо ее подписать "Свайпни меня"

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -21,3 +21,26 @@ const swiper = new Swiper('.swiper', {
     limitRotation: true,
   },
 });
+const swiper1 = new Swiper('.swiper1', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true,
+    // type: 'custom',
+    // bulletClass: 'swiper_pagination_img',
+  },
+  // autoplay: {
+  //   delay: 6000,
+  //   stopOnLastSlide: true,
+  //   disableOnInteraction: false,
+  // },
+  effect: 'flip',
+  flipEffect: {
+    slideShadows: true,
+    limitRotation: true,
+  },
+});
