5e6a6d2c8f68ae61f75a5a7255e9012f6e183eac
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2021-11-16 17:57:46 +0200

    button totop change and done
    Закончена кнопка go to top. Удален лишний текст который был закоминчен

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -22,22 +22,14 @@ const swiper = new Swiper('.swiper', {
   },
 });
 const swiper1 = new Swiper('.swiper1', {
-  // Optional parameters
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     dynamicBullets: true,
     clickable: true,
-    // type: 'custom',
-    // bulletClass: 'swiper_pagination_img',
   },
-  // autoplay: {
-  //   delay: 6000,
-  //   stopOnLastSlide: true,
-  //   disableOnInteraction: false,
-  // },
+ 
   effect: 'flip',
   flipEffect: {
     slideShadows: false,
